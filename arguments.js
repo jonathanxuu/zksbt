3807f5eccd6f269279133705b6ff337929d09b11
J <1410101208@qq.com>
2023-05-13 01:04:08 +0800

    Update function removeAssertionMethod

---
+++ arguments.js
@@ -0,0 +1,3 @@
+module.exports = [
+    ["0x11f8b77F34FCF14B7095BF5228Ac0606324E82D1"]
+  ];
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,16 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-ethers");
+const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const proxyAgent = new ProxyAgent("http://127.0.0.1:7890");
+setGlobalDispatcher(proxyAgent);
 
 module.exports = {
+  etherscan: {
+    apiKey: {
+      optimisticGoerli: process.env.ETHSCAN_API_KEY
+    }
+  },
   solidity: {
     version: "0.8.17",
     settings: {
@@ -15,15 +23,19 @@ module.exports = {
     },
   },
   networks: {
-    op_testnet: {
+    optimisticGoerli: {
       allowUnlimitedContractSize: true,
-      url: process.env.QUICKNODE_API_KEY_URL,
+      // url: process.env.QUICKNODE_API_KEY_URL,
+      url: `https://opt-goerli.g.alchemy.com/v2/H91sFaLoYh4VwBsHajIRRpOSc1CKawJm`,
+      chainId: 420,
+      live: true,
       accounts: [process.env.GOERLI_PRIVATE_KEY],
     },
     hardhat: {
-      accounts: {mnemonic: "increase help fortune noise jelly bronze hand among like powder crowd swamp"}
+      accounts: {mnemonic: process.env.MNEMONIC_ALICE}
     },
   },
+
   allowUnlimitedContractSize: true,
   
 };
